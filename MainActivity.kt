0e11f61ecd8f292c02280a21b159a4aba6624fd7
kyungjung kim <kj.kim@live.co.kr>
2019-12-18 07:55:43 +0900

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.test2
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ListView
-import android.widget.TabHost
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 
@@ -24,52 +24,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Create a BaseAdapter instance.
-//        val customBaseAdapter: BaseAdapter = object : BaseAdapter() {
-//            // Return list view item count.
-//            override fun getCount(): Int {
-//                return 4
-//            }
-//
-//            override fun getItem(i: Int): Any? {
-//                return null
-//            }
-//
-//            override fun getItemId(i: Int): Long {
-//                return 0
-//            }
-//
-//            override fun getView(itemIndex: Int, itemView: View?, viewGroup: ViewGroup?): View? {
-//                var itemView2: View? = itemView
-//                if (itemView2 == null) { // First inflate the RelativeView object.
-//                    itemView2 = LayoutInflater.from(this@MainActivity).inflate(null, null)
-//                }
-
-//                val titleView = itemView!!.findViewById(R.id.textView) as TextView
-//                if (0 == itemIndex) {
-//                    titleView.text = "All"
-//                } else if (1 == itemIndex) {
-//                    titleView.text = "1"
-//                } else if (2 == itemIndex) {
-//                    titleView.text = "2"
-//                } else if (3 == itemIndex) {
-//                    titleView.text = "3"
-//                }
-//
-//                val lableView = itemView.findViewById(R.id.lableView) as TextView
-//                lableView.text = "OFF"
-//
-//                return itemView
-//            }
-//        }
-//
-//
         lvNavList = findViewById(R.id.lv_activity_main_nav_list) as ListView
 
-//        // Set the custom base adapter to it.
-//        lvNavList!!.adapter = customBaseAdapter
+        // Set the custom base adapter to it.
+        lvNavList!!.adapter = customBaseAdapter(this)
 
-//        flContainer = findViewById(R.id.fl_activity_main_container) as FrameLayout
+        flContainer = findViewById(R.id.fl_activity_main_container) as FrameLayout
 
 
         dlDrawerLayout = findViewById(R.id.dl_activity_main_drawerlayout) as DrawerLayout
@@ -79,45 +39,91 @@ class MainActivity : AppCompatActivity() {
                 dlDrawerLayout?.openDrawer(lvNavList!!)
             }
         })
-//
-//
-//        lvNavList?.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
-//            val lableView = view.findViewById(R.id.lableView) as TextView
-//            if (0 == position) {
-//                if (lableView.text == "OFF") {
-//                    lableView.text = "ON"
-//
-//                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(1), 1, lvNavList!!.getChildAt(1).id.toLong())
-//                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(2), 2, lvNavList!!.getChildAt(2).id.toLong())
-//                    lvNavList?.performItemClick(lvNavList!!.getChildAt(3), 3, lvNavList!!.getChildAt(3).id.toLong())
-//                } else if (lableView.text == "ON") {
-//                    lableView.text = "OFF"
-//
-//                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(1),1, lvNavList!!.getChildAt(1).id.toLong())
-//                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(2), 2, lvNavList!!.getChildAt(2).id.toLong())
-//                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(3), 3, lvNavList!!.getChildAt(3).id.toLong())
-//                }
-//            } else if (1 == position) {
-//                if (lableView.text == "OFF") {
-//                    lableView.text = "ON"
-//                } else if (lableView.text == "ON") {
-//                    lableView.text = "OFF"
-//                }
-//            } else if (2 == position) {
-//                if (lableView.text == "OFF") {
-//                    lableView.text = "ON"
-//                } else if (lableView.text == "ON") {
-//                    lableView.text = "OFF"
-//                }
-//            } else if (3 == position) {
-//                if (lableView.text == "OFF") {
-//                    lableView.text = "ON"
-//                } else if (lableView.text == "ON") {
-//                    lableView.text = "OFF"
-//                }
-//            }
-//            //                dlDrawerLayout.closeDrawer(lvNavList);
-//        })
+
+
+        lvNavList?.setOnItemClickListener(AdapterView.OnItemClickListener { parent, view, position, id ->
+            val lableView = view.findViewById(R.id.lableView) as TextView
+
+            if (0 == position) {
+                isAll = true;
+
+                if (lableView.getText().equals("OFF")) {
+                    allState = true;
+
+                    lableView.text = "ON"
+
+                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(1), 1, lvNavList!!.getChildAt(1).id.toLong())
+                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(2), 2, lvNavList!!.getChildAt(2).id.toLong())
+                    lvNavList?.performItemClick(lvNavList!!.getChildAt(3), 3, lvNavList!!.getChildAt(3).id.toLong())
+                } else if (lableView.getText().equals("ON")) {
+                    allState = false;
+
+                    lableView.text = "OFF"
+
+                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(1),1, lvNavList!!.getChildAt(1).id.toLong())
+                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(2), 2, lvNavList!!.getChildAt(2).id.toLong())
+                    lvNavList!!.performItemClick(lvNavList!!.getChildAt(3), 3, lvNavList!!.getChildAt(3).id.toLong())
+                }
+
+                isAll = false;
+            } else if (1 == position) {
+                if (isAll == true) {
+                    if (allState == true) {
+                        if (lableView.getText().equals("OFF")) {
+                            lableView.setText("ON");
+                        }
+                    } else {
+                        if (lableView.getText().equals("ON")) {
+                            lableView.setText("OFF");
+                        }
+                    }
+                } else {
+                    if (lableView.getText().equals("OFF")) {
+                        lableView.setText("ON");
+                    } else if (lableView.getText().equals("ON")) {
+                        lableView.setText("OFF");
+                    }
+                }
+            } else if (2 == position) {
+                if (isAll == true) {
+                    if (allState == true) {
+                        if (lableView.getText().equals("OFF")) {
+                            lableView.setText("ON");
+                        }
+                    } else {
+                        if (lableView.getText().equals("ON")) {
+                            lableView.setText("OFF");
+                        }
+                    }
+                } else {
+                    if (lableView.getText().equals("OFF")) {
+                        lableView.setText("ON");
+                    } else if (lableView.getText().equals("ON")) {
+                        lableView.setText("OFF");
+                    }
+                }
+            } else if (3 == position) {
+                if (isAll == true) {
+                    if (allState == true) {
+                        if (lableView.getText().equals("OFF")) {
+                            lableView.setText("ON");
+                        }
+                    } else {
+                        if (lableView.getText().equals("ON")) {
+                            lableView.setText("OFF");
+                        }
+                    }
+                } else {
+                    if (lableView.getText().equals("OFF")) {
+                        lableView.setText("ON");
+                    } else if (lableView.getText().equals("ON")) {
+                        lableView.setText("OFF");
+                    }
+                }
+            }
+            //                dlDrawerLayout.closeDrawer(lvNavList);
+        })
+
 
         val tabHost1 = findViewById(R.id.tabHost1) as TabHost
         tabHost1.setup()
@@ -139,5 +145,41 @@ class MainActivity : AppCompatActivity() {
         ts3.setContent(R.id.content3)
         ts3.setIndicator("TAB 3")
         tabHost1.addTab(ts3)
+    }
+
+    private class customBaseAdapter(val context: Context) : BaseAdapter() {
+
+        // Return list view item count.
+        override fun getCount(): Int {
+            return 4
+        }
+
+        override fun getItem(i: Int): Any? {
+            return null
+        }
+
+        override fun getItemId(i: Int): Long {
+            return 0
+        }
+
+        override fun getView(itemIndex: Int, itemView: View?, viewGroup: ViewGroup?): View? {
+                var view: View = LayoutInflater.from(context).inflate(R.layout.activity_list_view_base_adapter, null)
+
+            val titleView = view.findViewById(R.id.textView) as TextView
+            if (0 == itemIndex) {
+                titleView.text = "All"
+            } else if (1 == itemIndex) {
+                titleView.text = "1"
+            } else if (2 == itemIndex) {
+                titleView.text = "2"
+            } else if (3 == itemIndex) {
+                titleView.text = "3"
+            }
+
+            val lableView = view.findViewById(R.id.lableView) as TextView
+            lableView.text = "OFF"
+
+            return view
+        }
     }
 }
